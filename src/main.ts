d0a4f7c972473c96b3a400126ffe564da24b605b
gap1994 <396912223@qq.com>
2021-06-18 10:37:42 +0800

    初步整合vant

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { Button } from 'vant'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(Button).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { UserConfigExport } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import styleImport from 'vite-plugin-style-import'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default (): UserConfigExport => {
+  return {
+    plugins: [
+      vue(),
+      styleImport({
+        libs: [
+          {
+            libraryName: 'vant',
+            esModule: true,
+            resolveStyle: (name) => `vant/es/${name}/style/index`
+          }
+        ]
+      })
+    ]
+  }
+}
