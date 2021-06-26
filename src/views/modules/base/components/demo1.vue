b77c488957e1fa214024d8ec5a3e5e9fb116f15b
gap1994 <396912223@qq.com>
2021-06-26 17:44:17 +0800

    开始做demo学习

---
+++ src/views/modules/base/components/demo1.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <hljs :code="html"/>
+  <hljs :code="js"/>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'demo1',
+  setup() {
+    const html = `//数据绑定
+<div id="counter">
+  Counter: {{ counter }}
+</div>`
+    const js = `const Counter = {
+  data() {
+    return {
+      counter: 0
+    }
+  }
+}`
+    return {
+      html,
+      js
+    }
+  }
+})
+</script>
--- src/views/modules/base/index.vue
+++ src/views/modules/base/index.vue
@@ -1,32 +1,18 @@
 <template>
-  <div>
-    <hljs
-      autodetect
-      :code="content"
-    />
-    <hljs autodetect :code="s"/>
-  </div>
+  <demo1/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import demo1 from './components/demo1.vue'
+
 export default defineComponent({
   name: 'baseVue',
+  components: {
+    demo1
+  },
   setup: () => {
-    let content = `<div id="counter" class="box">Counter: {{ counter }}</div>`
-    let s = `const Counter = {
-  data() {
-    return {
-      counter: 0
-    }
-  }
-}
-
-Vue.createApp(Counter).mount('#counter')`
-    return {
-      content,
-      s
-    }
+    return {}
   }
 })
 </script>
