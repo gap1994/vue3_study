173708c41b2522c88b281e1ece615394263dc99e
gap1994 <396912223@qq.com>
2021-06-28 09:41:43 +0800

    完善demo1

--- src/views/modules/base/components/demo1.vue
+++ src/views/modules/base/components/demo1.vue
@@ -1,26 +1,72 @@
 <template>
-
-  <hljs :code="html"/>
-  <hljs :code="js"/>
-
+  <div class="root">
+    <h1>声明渲染</h1>
+    <hljs :code="html"/>
+    <hljs :code="js"/>
+    <h1>例子</h1>
+    <div id="counter">
+      Counter: {{ counter }}
+    </div>
+    <van-button
+      type="primary"
+      @click="stopTimer"
+    >停止</van-button>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted } from 'vue'
 export default defineComponent({
   name: 'demo1',
+  data() {
+    return {
+      counter: 0,
+      inter: {} as NodeJS.Timeout
+    }
+  },
+  mounted() {
+    this.inter = setInterval(() => {
+      this.counter++
+    }, 1000)
+  },
+  methods: {
+    stopTimer() {
+      if (this.inter) {
+        clearInterval(this.inter)
+      }
+    }
+  },
   setup() {
     const html = `//数据绑定
 <div id="counter">
   Counter: {{ counter }}
-</div>`
+</div>
+<van-button
+  type="primary"
+  @click="stopTimer"
+>停止</van-button>`
     const js = `const Counter = {
   data() {
     return {
-      counter: 0
+      counter: 0,
+      inter: {} as NodeJS.Timeout
+    }
+  },
+  mounted() {
+    this.inter = setInterval(() => {
+      this.counter++
+    }, 1000)
+  },
+  methods: {
+    stopTimer() {
+      if (this.inter) {
+        clearInterval(this.inter)
+      }
     }
   }
-}`
+}
+Vue.createApp(Counter).mount('#counter')`
+
     return {
       html,
       js
@@ -28,3 +74,8 @@ export default defineComponent({
   }
 })
 </script>
+<style scoped>
+.root {
+  padding: 4vw;
+}
+</style>
